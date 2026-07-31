67a967032000f6514c615249ee9e16b6d4f506bb
Msserwano <sserwanomoris@gmail.com>
2026-08-01 00:08:52 +0300

    fixed

--- frontend/src/pages/applicant/ApplyWizard.jsx
+++ frontend/src/pages/applicant/ApplyWizard.jsx
@@ -1,5 +1,5 @@
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useParams, useNavigate, Link } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
 import Breadcrumbs from "../../components/layout/Breadcrumbs";
